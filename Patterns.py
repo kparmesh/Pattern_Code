e0e4e31436c8a5291269e3b42fdec0087e8a334c
Parmesh Kumawat <kumawatparmesh23@gmail.com>
2022-03-02 23:22:30 +0530

    Adding comments

---
+++ Patterns.py
@@ -0,0 +1,14 @@
+n= int(input("Please Enter the number of rows: "))
+for i in range(1,2*n):
+    if i<=n:
+        for spaces in range(1,n-i+1):
+            print(" ",end='')
+        
+        for j in range(1,i+1):
+            print('*',end=' ')
+    else:
+        for spaces in range(1,i-n+1):
+            print(" ",end='')
+        for j in range(n,i-n,-1):
+            print("*",end=" ")
+    print()
